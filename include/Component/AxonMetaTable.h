71deddf28b5edddac688db346e60bb0a43f92922
Sunghwa <bwshl27@postech.ac.kr>
2017-07-01 23:01:37 +0900

    Add axon metadata table source code

---
+++ include/Component/AxonMetaTable.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#include <TSim/Module/Module.h>
+#include <TSim/Simulation/Testbench.h>
+
+#include <Component/SRAMModule.h>
+
+#include <cinttypes>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class AxonMetaTable: public Module
+{
+public:
+    AxonMetaTable (string iname, Component *parent, uint32_t row_size);
+    virtual void Operation (Message **inmsgs, Message **outmsgs, 
+            const uint32_t *outque_size, Instruction *instr);
+
+private:
+    /* Port IDs */
+    // Read port  
+    uint32_t RPORT_addr;
+    uint32_t RPORT_data;
+
+    /* SRAM features */
+    uint32_t row_size_;
+    uint32_t col_size_;
+
+    /* Statistics */
+    uint32_t read_n, write_n;
+
+};
---
+++ src/Component/AxonMetaTable.cpp
@@ -0,0 +1,40 @@
+#include <Component/AxonMetaTable.h>
+
+#include <Message/IndexMessage.h>
+#include <Message/AxonMessage.h>
+
+#include <TSim/Utility/Prototype.h>
+#include <TSim/Utility/Logging.h>
+
+AxonMetaTable::AxonMetaTable (string iname, Component* parent,
+        uint32_t row_size)
+    : Module ("AxonMetaTable", iname, parent, 1)
+{
+
+     RPORT_addr = CreatePort ("r_addr", Module::PORT_INPUT,
+             Prototype<IndexMessage>::Get());
+     RPORT_data = CreatePort ("r_data", Module::PORT_OUTPUT,
+             Prototype<AxonMessage>::Get());
+
+     this->row_size_ = row_size;
+     this->col_size_ = 8; // bits
+
+     this->read_n = 0;
+     this->write_n = 0;
+}
+
+
+void AxonMetaTable::Operation (Message **inmsgs, Message **outmsgs, 
+        const uint32_t *outque_size, Instruction *instr)
+{
+    /* Read */
+    IndexMessage *raddr_msg = static_cast<IndexMessage*>(inmsgs[RPORT_addr]);
+
+    if(raddr_msg)
+    {
+        uint32_t read_addr = raddr_msg->value;
+        outmsgs[RPORT_data] = new AxonMessage (0, 0, 1);
+
+        DEBUG_PRINT("[SRAM] Receive read request, and send message");
+    }
+}
