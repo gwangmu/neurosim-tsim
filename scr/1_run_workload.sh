752f5b4376cc9a11d09721a36e2a63c7a8789c19
Gwangmu Lee <iss300@postech.ac.kr>
2017-07-29 09:52:36 +0900

    merging

--- scr/1_run_workload.sh
+++ scr/1_run_workload.sh
@@ -1,10 +1,9 @@
 #! /bin/bash
 
 mkdir -p result
-j=0
-for i in 10 25 64 200 
+for i in 10 25 64 200
 do
-    f=r0"$j".isp"$i"k
+    f=isp_"$i"k
     echo "Simulate $f workloads" 
 
     cd simspec
@@ -15,7 +14,5 @@ do
 
     ./neurosim-tsim simspec/neurosim.simspec -p 0 > result/"$f".result \
         2> result/"$f".err
-
-    j=$((j+1))
 
 done
